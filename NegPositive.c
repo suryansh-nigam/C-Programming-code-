74a5fd6422cfea53fd38601db5eff98736811fcf
Suryansh Nigam <131799265+suryansh-nigam@users.noreply.github.com>
2025-09-24 19:36:39 +0530

    NegPositive.c

---
+++ NegPositive.c
@@ -0,0 +1,16 @@
+Q - Finding given no is positive or negative or zero
+
+#include <stdio.h>
+
+void main (){
+
+int n;
+printf("Enter a value");
+scanf("%d", &n);
+
+n == 0 && puts("It is zero");
+n > 0 && puts("It is positive");
+n < 0 && puts("Its is negative");
+
+
+}
