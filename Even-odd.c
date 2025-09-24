1603531a37161a428dee94c570322f4987655c26
Suryansh Nigam <131799265+suryansh-nigam@users.noreply.github.com>
2025-09-24 19:26:53 +0530

    Even-odd.c

---
+++ Even-odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main (){
+    int n ;
+    printf("Enter n value");
+    scanf("%d", &n);
+    
+    n%2 == 0 && puts( Even");
+    n%2 != 0 && puts("odd no");
+    
+}
+
+Output - Enter n value11
+odd no
+      - Enter n value12
+even 
