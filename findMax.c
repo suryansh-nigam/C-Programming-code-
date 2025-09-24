9620bd95d9aa12bf6a74153681a46df8a8a05871
Suryansh Nigam <131799265+suryansh-nigam@users.noreply.github.com>
2025-09-24 19:32:24 +0530

    findMax.c

---
+++ findMax.c
@@ -0,0 +1,17 @@
+Q - Finding max in 2 numbers without using if else statement
+
+#include <stdio.h>
+
+void main (){
+
+int a,b;
+printf("Enter a value");
+scanf("%d", &a);
+printf("Enter b value");
+scanf("%d", &b);
+
+a>b && puts("a is big ");
+b>a && puts("b is big");
+a== b && puts("It is equal");
+
+}
