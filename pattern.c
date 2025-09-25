9f95b12306e44d870df68978a38138d726371af7
Suryansh Nigam <131799265+suryansh-nigam@users.noreply.github.com>
2025-09-25 09:33:59 +0530

    pattern.c

---
+++ pattern.c
@@ -0,0 +1,21 @@
+Q5 - write a c program to print following pattern using printf() function only.
+
+# # # # #
+
+#       #
+
+#       #
+
+# # # # #
+
+
+#include <stdio.h>
+
+void main() {
+    
+    printf(" # # # # #\n");
+    printf(" #       #\n");
+    printf(" #       #\n");
+    printf(" # # # # #\n");
+
+}
