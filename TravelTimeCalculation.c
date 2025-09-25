f756c635f1c2039caff21fc882ce02784e800779
Suryansh Nigam <131799265+suryansh-nigam@users.noreply.github.com>
2025-09-25 19:30:08 +0530

    TravelTimeCalculation.c

---
+++ TravelTimeCalculation.c
@@ -0,0 +1,17 @@
+Q -Travel Time Calculation
+You travel 150 km with an average speed of 60 km/h.
+How much time will it take?
+
+code  - 
+#include <stdio.h>
+
+void main() {
+    float distance = 150.0;
+    float speed = 60;
+    float time = distance/time;
+    printf("Trave time for %.1f km  at %.1f km/h is &.2f hours\n",
+    distance,speed,time);
+
+}
+
+output - Trave time for 150.0 km  at 60.0 km/h is &.2f hours
