685ee7ac72c849040d619c01c581cf9f1b10c6a8
Suryansh Nigam <131799265+suryansh-nigam@users.noreply.github.com>
2025-11-06 19:38:07 +0530

    Add OddPattern.c to print even numbers in a pattern
    Implement a pattern printing program for even numbers.

---
+++ OddPattern.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+  
+  
+  
+  int n = 4;
+  int a = 2;// for even
+
+  
+  for(int i=1; i<=n; i++){
+      for(int j=1; j<=n+1; j++){
+          printf("%3d", a);
+          a= a+2;
+      }
+      printf("\n");
+  }
+   return 0;
+}
