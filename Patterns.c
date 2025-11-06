76e238f09247b95f906a493523524456d1302f49
Suryansh Nigam <131799265+suryansh-nigam@users.noreply.github.com>
2025-11-06 21:43:06 +0530

    Add program to print multiplication table
    This program prints a multiplication table for numbers 1 to 5.

---
+++ Patterns.c
@@ -0,0 +1,23 @@
+What is the output of this program?
+
+
+1 2 3 4 5
+2 4 6 8 10
+3 6 9 12 15
+4 8 12 16 20
+5 10 15 20 25
+
+===============================================================================
+
+#include <stdio.h>
+int main() {
+    
+    for(int i = 1; i <= 5; i++) {
+        for(int j = 1; j <= 5; j++) {
+           
+            printf("%d ", i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
